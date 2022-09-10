d69d85417257700a77ed4f1128024365e8fae538
DanielC17 <danielccabral1981@gmail.com>
2022-09-10 00:13:07 -0300

    criando class java exame com setter

---
+++ src/main/java/Exame.java
@@ -0,0 +1,32 @@
+public class Exame {
+    private int Triglicerides;
+    private int ColesterolTotal;
+    private int ColesterolHDL;
+    private int ColesterolLDL;
+    private int ColesterolVLDL;
+    private int Glicose;
+
+    public void setTriglicerides(int triglicerides) {
+        Triglicerides = triglicerides;
+    }
+
+    public void setColesterolTotal(int colesterolTotal) {
+        ColesterolTotal = colesterolTotal;
+    }
+
+    public void setColesterolHDL(int colesterolHDL) {
+        ColesterolHDL = colesterolHDL;
+    }
+
+    public void setColesterolLDL(int colesterolLDL) {
+        ColesterolLDL = colesterolLDL;
+    }
+
+    public void setColesterolVLDL(int colesterolVLDL) {
+        ColesterolVLDL = colesterolVLDL;
+    }
+
+    public void setGlicose(int glicose) {
+        Glicose = glicose;
+    }
+}
